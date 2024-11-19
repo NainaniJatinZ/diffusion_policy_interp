4df573ffb26f89860859fd56446d7ef9214b9bb1
NainaniJatinZ <nainani.jatin.0@gmail.com>
2024-11-19 05:18:51 +0000

    added functions to characterize latents

--- sae_analysis/discrete_obs.py
+++ sae_analysis/discrete_obs.py
@@ -116,11 +116,7 @@ with tqdm(total=max_steps, desc=f"Seed {target_seed} Eval") as pbar:
     
             policy.noise_scheduler.set_timesteps(policy.num_inference_steps)
 
-            # inference_inputs[step_idx] = []
-            if step_idx == target_step:
-                target_obs = cond.clone().cpu().numpy()
-                target_step_image = env.render(mode='rgb_array')
-                print(f"Step {step_idx} Image")
+            
             for t in policy.noise_scheduler.timesteps:
                 trajectory[cond_mask] = cond_data[cond_mask]
                 # step_input = {
@@ -147,6 +143,13 @@ with tqdm(total=max_steps, desc=f"Seed {target_seed} Eval") as pbar:
             start = To - 1
             end = start + policy.n_action_steps
             action = action_pred[:, start:end]
+
+            # inference_inputs[step_idx] = []
+            if step_idx == target_step:
+                target_obs = cond.clone().cpu().numpy()
+                target_step_image = env.render(mode='rgb_array')
+                target_action = action.clone().cpu().numpy()
+                print(f"Step {step_idx} Image")
 
         naction = action.detach().to('cpu').numpy()
         action = naction[0]
@@ -188,7 +191,7 @@ points = np.array(obs.detach().cpu()).reshape(-1, 2)
 
 # Rescale points from [0, 512] to [0, 96]
 points = points / 512 * 96
-
+target_action_norm = target_action[0] / 512 * 96
 # Center of the image
 center_x, center_y = 96 / 2, 96 / 2
 
@@ -209,8 +212,14 @@ for idx, (x, y) in enumerate(points):
     plt.scatter(x, y, c='red', label=f'Point {idx}' if idx == 0 else None)  # Points now match image size
     plt.text(x, y, f'{idx}', fontsize=9, color='blue')
 
+for idx, (x, y) in enumerate(target_action_norm):
+    plt.scatter(x, y, c='yellow', label=f'Point {idx}' if idx == 0 else None)  # Points now match image size
+    plt.text(x, y, f'{idx}', fontsize=9, color='red')
+
 # Plot the center of the image
 plt.scatter(center_x, center_y, c='green', label='Center')  # Center of the image
+
+# plt.scatter(1, 1, c='yellow', label='Action')  # Center of the image
 
 # Draw the line
 plt.plot([center_x, end_x], [center_y, end_y], c='purple', label=f'Line d_i={d_i}, angle=pi/4')
@@ -219,8 +228,99 @@ plt.legend(loc='upper left')
 plt.title("Points and Line Overlay on Image")
 plt.show()
 
+# %%
+
+mid_pt = points[3]
+low_t = points[4]
+curr_block_theta = np.arctan2(- mid_pt[1] + low_t[1], mid_pt[0] - low_t[0])
+print(curr_block_theta)
+print(curr_block_theta * 180 / np.pi)
+
+# %%
+
+def theta_tc(obs):
+    """
+    Calculates the angle between the target block and the current block
+    """
+    mid_pt = obs[3]
+    low_t = obs[4]
+    curr_block_theta = np.arctan2(- mid_pt[1] + low_t[1], mid_pt[0] - low_t[0])
+    theta_tc = curr_block_theta - np.pi / 4
+    return theta_tc, theta_tc * 180 / np.pi, curr_block_theta, curr_block_theta * 180 / np.pi
+
+theta_tc(points)
+
+# %%
+
+def dist_tc(obs):
+    """
+    Calculates the distance between the target block and the current block
+    """
+    mid_pt_current = obs[3]
+    mid_pt_target = [48, 48]
+    dist = np.sqrt((mid_pt_current[0] - mid_pt_target[0])**2 + (mid_pt_current[1] - mid_pt_target[1])**2)
+    return dist
+
+dist_tc(points)
+
+# %%
+
+def Ka(obs):
+    """
+    Calculates the closest keypoint to agent """
+    agent = obs[-1]
+    min_dist = np.inf
+    min_idx = -1
+    for idx, (x, y) in enumerate(obs):
+        if idx == 9:
+            continue
+        else:
+            dist = np.sqrt((x - agent[0])**2 + (y - agent[1])**2)
+            if dist < min_dist:
+                min_dist = dist
+                min_idx = idx
+    return min_idx
+
+Ka(points)
 
 
 # %%
-image
+
+def theta_action(action):
+    """
+    Calculates the change in angle of the first and last action
+    """
+    first_angle = np.arctan2(- action[0][1] + action[1][1], action[0][0] - action[1][0])
+    last_angle = np.arctan2(- action[-2][1] + action[-1][1], action[-2][0] - action[-1][0])
+    change = last_angle - first_angle
+    return first_angle, last_angle, change, change * 180 / np.pi
+
+theta_action(target_action_norm)
+
+# %%
+
+def dist_action(action):
+    """
+    Calculates the distance between the first and last action
+    """
+    dist = np.sqrt((action[0][0] - action[-1][0])**2 + (action[0][1] - action[-1][1])**2)
+    return dist
+
+dist_action(target_action_norm)
+
+
+# %%
+
+def dist_action_mid(action, obs):
+    """
+    Calculates the average distance between actions and midpoint of target block
+    """
+    mid_pt_target = obs[3]
+    dist = 0
+    for idx, (x, y) in enumerate(action):
+        dist += np.sqrt((x - mid_pt_target[0])**2 + (y - mid_pt_target[1])**2)
+    return dist / len(action)
+
+dist_action_mid(target_action_norm, points)
+    
 # %%
